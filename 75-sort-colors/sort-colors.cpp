74b0656435540e5dbff01fc452f43fc5aa69995f
Sumeet Kumar Sahoo <93501013+Sumeet2930@users.noreply.github.com>
2026-01-22 11:55:19 +0530

    Time: 3 ms (2.77%) | Memory: 11.7 MB (46.97%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n; i++){
+            cout << nums[i] <<",";
+        }
+        return;
+    }
+};
