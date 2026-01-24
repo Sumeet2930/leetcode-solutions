f3002371620bef90c4888d88f182d1bb9cae44e9
Sumeet Kumar Sahoo <93501013+Sumeet2930@users.noreply.github.com>
2026-01-24 13:52:52 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (76.39%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -8,17 +8,17 @@ public:
         int high = m * n - 1;
 
         while (low <= high) {
-            int mid = low + (high - low) / 2;
+            int mid = low + (high-low)/2;
+            int row = mid/n;
+            int col = mid%n;
 
-            int row = mid / n;
-            int col = mid % n;
-
-            if (matrix[row][col] == target)
+            if (matrix[row][col]==target){
                 return true;
-            else if (matrix[row][col] < target)
+            }else if(matrix[row][col] < target){
                 low = mid + 1;
-            else
+            }else{
                 high = mid - 1;
+            }
         }
         return false;
     }
