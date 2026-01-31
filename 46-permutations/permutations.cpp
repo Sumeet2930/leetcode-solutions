a59d2d06c6999c9ac6a2b70bf945d5f2d71d6c05
Sumeet Kumar Sahoo <93501013+Sumeet2930@users.noreply.github.com>
2026-01-31 10:55:09 +0530

    Time: 3 ms (28.06%) | Memory: 11 MB (23.24%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> temp;
+    vector<bool> used;
+
+    void backtrack(vector<int>& nums) {
+
+        // Base case: one permutation pei complete kar dega
+        if (temp.size() == nums.size()) {
+            ans.push_back(temp);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (used[i]) continue;
+
+            used[i] = true;
+            temp.push_back(nums[i]);
+
+            backtrack(nums);
+
+            temp.pop_back();
+            used[i] = false;
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        used.resize(nums.size(), false);
+        backtrack(nums);
+        return ans;
+    }
+};
