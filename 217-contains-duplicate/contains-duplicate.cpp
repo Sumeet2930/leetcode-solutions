cd486dd6bfed8122ca1bf742089c495e5e3de3b9
Sumeet Kumar Sahoo <93501013+Sumeet2930@users.noreply.github.com>
2026-02-06 11:13:55 +0530

    Time: 61 ms (21.18%) | Memory: 90.7 MB (69.18%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -3,14 +3,14 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         int n = nums.size();
 
-        sort(nums.begin(), nums.end());
+        unordered_map<int,int> freq;
 
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
-                return true;  
-            }
+        for (int i=0; i<n; i++){
+            freq[nums[i]]++;
+
+            if (freq[nums[i]] > 1) return true;
         }
 
-        return false;  
+        return false;
     }
 };
