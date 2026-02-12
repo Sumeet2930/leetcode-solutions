c3cd7dfd70ba9be94962afcb5f1b2bc1ce3deef0
Sumeet Kumar Sahoo <93501013+Sumeet2930@users.noreply.github.com>
2026-02-12 10:00:07 +0530

    Time: 3 ms (27.39%) | Memory: 29 MB (38.91%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -22,8 +22,7 @@ public:
 
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
        
-        if(!root && !subRoot) return true;
-        if(!root || !subRoot) return false;      
+        if(!root) return false;      
         if(isSameTree(root, subRoot)) return true;
         return isSubtree(root->left ,subRoot) || isSubtree(root->right , subRoot);
     }
