bb0e9e91bf6fe60d7763e1ba10ef2948453cd007
Sumeet Kumar Sahoo <93501013+Sumeet2930@users.noreply.github.com>
2026-02-20 23:07:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (59.31%) - LeetSync

---
+++ 763-special-binary-string/special-binary-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string makeLargestSpecial(string s) {
+        vector<string> substrings;
+        int count = 0, start = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '1') count++;
+            else count--;
+
+            // When count becomes 0, we found a special substring
+            if (count == 0) {
+                // Recursively solve inner substring
+                string inner = makeLargestSpecial(s.substr(start + 1, i - start - 1));
+                substrings.push_back("1" + inner + "0");
+                start = i + 1;
+            }
+        }
+
+        // Sort in descending order
+        sort(substrings.begin(), substrings.end(), greater<string>());
+
+        string result;
+        for (string &str : substrings)
+            result += str;
+
+        return result;
+    }
+};
