373e4403852e0159c5251c55cb1be5a1c65007f8
Sumeet Kumar Sahoo <93501013+Sumeet2930@users.noreply.github.com>
2026-03-04 11:03:21 +0530

    Time: 3 ms (65.41%) | Memory: 8.3 MB (10.37%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,17 @@
+//I trying to solve this question using DP(Memoization)
+
+class Solution {
+public:
+    int fib(int n) {
+        vector<int> dp(n+1, -1);
+            return fibonacci(n, dp);
+        
+    }
+
+    int fibonacci(int n, vector<int>& dp){
+        if (n <= 1) return n;
+        if(dp[n] != -1) return dp[n];
+
+        return dp[n] = fibonacci(n-1, dp) + fibonacci(n-2, dp);
+    }
+};
