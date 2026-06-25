95236c65701dade1a95a53a4818d94c78e54fb28
Sumeet Kumar Sahoo <93501013+Sumeet2930@users.noreply.github.com>
2026-06-25 15:00:21 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (43.81%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -1,14 +1,12 @@
-#include <vector>
-
 class Solution {
 public:
-    std::vector<int> relativeSortArray(std::vector<int>& arr1, std::vector<int>& arr2) {
-        std::vector<int> count(1001, 0);
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        vector<int> count(1001, 0);
         for (int num : arr1) {
             count[num]++;
         }
         
-        std::vector<int> result;
+        vector<int> result;
         
         
         for (int num : arr2) {
